35b681d3e6e81524da0e4fc701c6a29306fbb93d
Sergey Orekhov <sergejorehov1992@gmail.com>
2022-10-14 15:40:49 +0300

    added scroll

--- oman-app/src/components/Footer/index.tsx
+++ oman-app/src/components/Footer/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+//navigation
+import { NavLink } from "react-router-dom";
+
 //components
+import { routeMain as routeMainPage } from "pages/MainPage";
+
+//scroll
+import { Link, animateScroll as scroll } from "react-scroll";
 
 //styles
 import "./styles.scss";
@@ -10,56 +17,79 @@ const Footer = () => {
   const telegramLogo = require("assets/telegram-logo.png");
   const youtubeLogo = require("assets/youtube-logo.png");
   const logoImage = require("assets/logoFooter.png");
+  const arrowUp = require("assets/arrowUp.png");
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="wraper">
-          <div className="logo">
-            <img src={logoImage} alt="logo" title="logo"></img>
+      <div className="mainFooter">
+        <div className="container">
+          <div className="wraper">
+            <NavLink to={routeMainPage()} activeClassName={"linkactive"}>
+              <div className="logo">
+                <img src={logoImage} alt="logo" title="logo"></img>
+              </div>
+            </NavLink>
+            <h3>Соц.сети</h3>
+            <div className="socialNetwork">
+              <a href="" className="socialNetwork__item">
+                <img src={facebookLogo} title="facebook" alt="logo"></img>
+              </a>
+              <a href="" className="socialNetwork__item">
+                <img src={youtubeLogo} title="youtube" alt="logo"></img>
+              </a>
+              <a href="" className="socialNetwork__item">
+                <img src={telegramLogo} title="telegram" alt="logo"></img>
+              </a>
+            </div>
           </div>
-          <div className="socialNetwork">
-            <a href="" className="socialNetwork__item">
-              <img src={facebookLogo} title="facebook" alt="logo"></img>
+          <div className="infoBlock">
+            <a className="phoneCall" href="tel:+380442949117">
+              +38 (044) 294 91 17
             </a>
-            <a href="" className="socialNetwork__item">
-              <img src={youtubeLogo} title="youtube" alt="logo"></img>
-            </a>
-            <a href="" className="socialNetwork__item">
-              <img src={telegramLogo} title="telegram" alt="logo"></img>
-            </a>
+            <a href="mailto:support@oman.ua">support@oman.ua</a>
+            <p className="workTime">Пн-Пт, 9.00 - 18:00</p>
+          </div>
+          <div className="aboutUs">
+            <h3>Информация о компании</h3>
+            <ul>
+              <li>О нас</li>
+              <li>Контакты</li>
+            </ul>
+          </div>
+          <div className="toClients">
+            <h3>Покупателям</h3>
+            <ul>
+              <li>Оплата</li>
+              <li>Доставка</li>
+              <li>Возврат</li>
+              <li>Купить</li>
+              <li>Монтаж</li>
+            </ul>
+          </div>
+          <div className="toPartners">
+            <h3>Партнерам</h3>
+            <ul>
+              <li>Строителям</li>
+              <li>Диллерам</li>
+            </ul>
           </div>
         </div>
-        <div className="infoBlock">
-          <a className="phoneCall" href="tel:+380442949117">
-            +38 (044) 294 91 17
+      </div>
+      <div className="copy">
+        <div className="container">
+          <p> © 2022 Все Права Защищены</p>
+          <a href="" className="arrowUp" onClick={scrollToTop}>
+            {" "}
+            <img src={arrowUp} alt="arrow" title="arrow"></img>
           </a>
-          <a href="mailto:support@oman.ua">support@oman.ua</a>
-          <p className="workTime">Пн-Пт, 9.00 - 18:00</p>
-        </div>
-        <div className="aboutUs">
-          <h3>Информация о компании</h3>
-          <ul>
-            <li>О нас</li>
-            <li>Контакты</li>
-          </ul>
-        </div>
-        <div className="toClients">
-          <h3>Покупателям</h3>
-          <ul>
-            <li>Оплата</li>
-            <li>Доставка</li>
-            <li>Возврат</li>
-            <li>Купить</li>
-            <li>Монтаж</li>
-          </ul>
-        </div>
-        <div className="toPartners">
-          <h3>Партнерам</h3>
-          <ul>
-            <li>Строителям</li>
-            <li>Диллерам</li>
-          </ul>
         </div>
       </div>
     </footer>
